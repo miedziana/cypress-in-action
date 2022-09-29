34a011c0eec1dacc3402c3ecf0ca1ceb7a6df4b7
Magdalena Sadlo <magdalena.sadlo@2BIT.ch>
2022-09-29 08:37:13 +0200

    move cypress tests to e2e folder

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,10 +1,8 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
-    supportFile: 'support/e2e.ts',
-    specPattern: 'e2e/*.spec.cy.ts',
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
       // include any other plugin code...
