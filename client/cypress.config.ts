62b91de64b97ead43c4e78c60782a483bd144aed
Magdalena Sadlo <magdalena.sadlo@2BIT.ch>
2022-09-29 10:44:40 +0200

    add component test config

---
+++ client/cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: '**/*.cy.ts',
+  },
+
+});
---
+++ client/src/app/components/footer/footer.component.cy.ts
@@ -0,0 +1,7 @@
+import {FooterComponent} from './footer.component';
+
+describe('footer.component.cy.ts', () => {
+  it('playground', () => {
+    cy.mount(FooterComponent);
+  });
+});
