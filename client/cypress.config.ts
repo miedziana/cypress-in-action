8b3f65002714dcebd3ca6ab40f76ef4f5c1b276a
Magdalena Sadlo <magdalena.sadlo@2BIT.ch>
2022-10-05 16:48:33 +0200

    add code coverage

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -4,7 +4,10 @@ export default defineConfig({
 
   e2e: {
     'baseUrl': 'http://localhost:4200',
-    supportFile: false
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    }
   },
 
 
